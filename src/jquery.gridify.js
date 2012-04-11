0497a3785a8312911556549b0313a93185ebb4a6
Jesse Panganiban <me@jpanganiban.com>
2012-04-11 19:29:01 +0800

    Accept options and render tiles

--- src/jquery.gridify.js
+++ src/jquery.gridify.js
@@ -1,6 +1,25 @@
 (function($) {
 
-$.fn.gridify = function() {
+$.fn.gridifyOptions = {
+  tileClassName: 'tile',
+  gridColumns: 10,
+  gridRows: 10
+}; 
+
+$.fn.gridify = function(options) {
+
+  var options = $.extend($.fn.gridifyOptions, options);
+
+  this.each(function() {
+    for(row=0; row<options.gridRows; row++) {
+      for(column=0; column<options.gridColumns; column++) {
+        $(this).append("<div class=" + options.tileClassName +
+          " row-" + row + " column-" + column + "></div>");
+      }
+      $(this).append("<br>");
+    }
+  });
+
 };
 
 })(jQuery);
